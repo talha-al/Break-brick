38db870ad197053d8711eba90a69613e78791b95
Talha Şeref Alataş <talhaalatas97@gmail.com>
2021-08-28 22:33:57 +0300

    Initial commit

---
+++ Assets/Scripts/TopKontrol.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopKontrol : MonoBehaviour
+{
+    public GameObject pedal;
+    Rigidbody2D topFizik;
+    bool oyunBaslangic = true;
+
+    void Start()
+    {
+        topFizik = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(oyunBaslangic)
+        {
+            transform.position = new Vector3(pedal.transform.position.x, transform.position.y, transform.position.z);
+
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            oyunBaslangic = false;
+            topFizik.velocity = Vector2.zero;
+            topFizik.AddForce(new Vector2(50, 800));
+        }
+
+    }
+}
---
+++ Assets/Scripts/Tuğla.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuğla : MonoBehaviour
+{
+    public static int toplamTuğla;
+    public Sprite[] tuğlaDizi;
+    SpriteRenderer spriteRenderer;
+    int maxCarpmaSayisi;
+    int carpmaSayisi=0;
+
+    void Start()
+    {
+        maxCarpmaSayisi = tuğlaDizi.Length+1;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        toplamTuğla++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name.Equals("top"))
+        {
+            carpmaSayisi++;
+            
+            if(carpmaSayisi>=maxCarpmaSayisi)
+            {
+                Destroy(this.gameObject);
+                toplamTuğla--;
+                if(toplamTuğla<=0)
+                {
+                    //  GameObject.FindObjectOfType<OyunKontrol>().GetComponent<OyunKontrol>().SahneDegis();
+                    GameObject.Find("OyunKontrol").GetComponent<OyunKontrol>().SahneDegis();
+                }
+            }
+            else
+            {
+                spriteRenderer.sprite = tuğlaDizi[carpmaSayisi - 1];
+                
+            }
+        }
+    }
+}
